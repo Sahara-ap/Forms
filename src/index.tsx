f3638108ebcd70a5b9cec32a786bd2fb1b61fa83
Sahara <sahara.ap13@gmail.com>
2024-08-10 15:39:12 +0300

    feat: adds Global and Reset styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { ResetStyles } from 'styles/ResetStyles';
+import { GlobalStyles } from 'styles/GlobalStyles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ResetStyles />
+    <GlobalStyles />
     <BrowserRouter>
       <App />
     </BrowserRouter>
